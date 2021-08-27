431e72af6b024cc33f22e1d7643b60ecebcace5c
Ievhen <81873297+evgeniy-konstantinov@users.noreply.github.com>
2021-08-27 11:19:26 +0300

    Update Block1.js

--- src/componets/Block1.js
+++ src/componets/Block1.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import Button from './button/button';
-import Rectangle from '../images/Rectangle.png';
 
 const Section = styled.section`
-  background-image: url(${Rectangle});
+  background-image: url(${props => props.background});
 
   height: 900px;
   display: flex;
